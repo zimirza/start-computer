c4f7b6243d41e62b9fd3b2215409ea4e5811e01c
Зишан Мирза <149377404+zimirza@users.noreply.github.com>
2024-09-13 15:58:17 +0200

    refactor

--- main.c
+++ main.c
@@ -4,19 +4,13 @@
 
 int main(int argc, char **argv) {
   if (argc != 2) {
-    printf("usage: start-computer <external_monitor|no_external_monitor>\n");
+    printf("usage: start-computer\n");
     exit(1);
   }
 
-  if (strcmp(argv[1], "external_monitor") == 0) {
-    if (system("xrandr --output eDP-1 --off") != 0) {
-      fprintf(stderr, "unable to run xrandr cmd");
-    }
-  }
-
-  int xrdb = system("xrdb ~/.config/Xresources");
+  int xrdb = system("xrdb ~/.Xresources");
   int fix_java = system("wmname LG3D");
 
-  printf("%d %d %d %d\n", xrdb, fix_java);
+  printf("%d %d\n", xrdb, fix_java);
   return 0;
 }
